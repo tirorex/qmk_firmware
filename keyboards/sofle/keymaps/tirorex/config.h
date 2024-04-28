50290a48ad13cc5d3d8f354acd3096f3813f6464
Yury Kardyukov <kardyukovya@ekassir.com>
2024-04-28 17:51:37 +0300

    actual

--- keyboards/sofle/keymaps/tirorex/config.h
+++ keyboards/sofle/keymaps/tirorex/config.h
@@ -33,3 +33,5 @@ for more options.
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
 #define TRI_LAYER_ADJUST_LAYER 4
+
+#define HOLD_ON_OTHER_KEY_PRESS
--- keyboards/sofle/keymaps/tirorex/keymap.c
+++ keyboards/sofle/keymaps/tirorex/keymap.c
@@ -187,11 +187,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [MAIN] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_RBRC,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    TT(HIGH),                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_RBRC,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MUTE,KC_N,    KC_M, KC_COMM,  CS_00, TD(TD_HARD_ZNAK), KC_SLSH,
-                 KC_LSFT,KC_LALT,KC_LCTL, TL_LOWR, KC_ENT,      KC_SPC,  TL_UPPR, KC_F13, KC_RALT, KC_RSFT
+  KC_GRV,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MUTE,KC_N,    KC_M, KC_COMM,  CS_00, TD(TD_HARD_ZNAK), KC_RBRC,
+                 KC_LSFT,KC_LALT,KC_LCTL, MO(LOW), KC_ENT,      KC_SPC,  LT(HIGH, KC_BSPC), KC_F13, KC_LGUI, KC_RSFT
 ),
 /* NUMPAD
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -211,13 +211,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,------------------------------------------------.                    ,---------------------------------------------------.
   TG(_NUMPAD), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   _______, KC_NUM,  XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_CIRC, KC_P7,  KC_P8,   KC_P9,   XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PPLS, KC_P7,  KC_P8,   KC_P9,   KC_PAST, XXXXXXX,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MINS, KC_P4,  KC_P5,   KC_P6,   XXXXXXX,  XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PMNS, KC_P4,  KC_P5,   KC_P6,   KC_PSLS,  XXXXXXX,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,   _______,KC_P0, KC_P1,  KC_P2,   KC_P3,   XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,   _______,KC_P0, KC_P1,  KC_P2,   KC_P3,   KC_EQL, XXXXXXX,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-              _______, XXXXXXX, _______, _______, _______,   _______, MO(HIGH), KC_P0 ,   KC_PDOT, _______
+              _______, XXXXXXX, _______, _______, _______,   _______, MO(HIGH), KC_P0 ,   KC_PDOT, KC_EQL
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
 ),
 /* LOWER
@@ -236,9 +236,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [LOW] = LAYOUT(
   CH_LANG,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  KC_CAPS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, TG(_NUMPAD), _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
+  S(KC_EQL), _LCBR,   _RCBR, _LT,  _GT, S(KC_MINS),                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+  _PIPE,  _BSLS, _LBRC, _RBRC, MO(ADJUST), TG(_NUMPAD), _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* RAISE
@@ -257,9 +257,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [HIGH] = LAYOUT(
   _______, _______ , _______ , _______ , _______ , _______,          _LCBR,  _RCBR  , _LT,  _GT ,  S(KC_MINS) ,S(KC_EQL),
-  _AMPR,  _ASTR,  _AT,   _HASH,  _DLR, _CIRC,                        KC_PGUP, _PIPE,   KC_UP, _LBRC,_RBRC, KC_RBRC,
+  _AMPR,  _ASTR,  _AT,   _HASH,  _DLR, _CIRC,                        KC_PGUP, _PIPE,   KC_UP, KC_HOME,KC_END, KC_RBRC,
   _SCLN, _DQUO,  _QUOT,  _QUES,_EXLM  , _COLN,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  KC_EQL, _PERC,S(KC_MINS), KC_MINS, _LPRN, _RPRN,  _______,       _BSLS,  QK_LEAD, _SLSH, XXXXXXX, _______,   XXXXXXX, _______,
+  KC_EQL, _PERC,S(KC_MINS), KC_MINS, _LPRN, _RPRN,  _______,       _BSLS,  QK_LEAD, _SLSH, KC_INS, _______,   XXXXXXX, _______,
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* ADJUST
@@ -277,7 +277,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_SLEP,                           XXXXXXX,  XXXXXXX  , XXXXXXX,  XXXXXXX ,  XXXXXXX ,XXXXXXX,
+  QK_RBT, XXXXXXX , XXXXXXX , XXXXXXX , EE_CLR , KC_SLEP,                           XXXXXXX,  XXXXXXX  , XXXXXXX,  XXXXXXX ,  XXXXXXX ,XXXXXXX,
   XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,XXXXXXX  , XXXXXXX,                       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,
@@ -398,7 +398,50 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
          }
         return true;
-    }        
+    }
+    case LT(HIGH, KC_BSPC):{
+        static bool delkey_registered;
+         if (record->event.pressed && record->tap.count){
+            if (mod_state & MOD_MASK_SHIFT ){
+                del_mods(MOD_MASK_SHIFT);
+                register_code(KC_DEL);
+                delkey_registered = true;
+                set_mods(mod_state);
+                return false;
+            }
+         }
+         else {
+            if (delkey_registered) {
+                unregister_code(KC_DEL);
+                delkey_registered = false;
+                return false;
+            }
+         }
+        return true;
+    }   
+    case KC_BSPC: {
+        static bool delkey_registered;
+         if (record->event.pressed){
+            print("KC_BSPC\n");
+            if (mod_state & MOD_MASK_SHIFT ){
+                print("MOD_MASK_SHIFT\n");
+                del_mods(MOD_MASK_SHIFT);
+                register_code(KC_DEL);
+                delkey_registered = true;
+                set_mods(mod_state);
+                return false;
+            }
+         }
+         else {
+            if (delkey_registered) {
+                unregister_code(KC_DEL);
+                delkey_registered = false;
+                return false;
+            }
+         }
+        return true;
+    }   
+       
     //Обработка точки.
     case CS_00:{
         if (record->event.pressed){
